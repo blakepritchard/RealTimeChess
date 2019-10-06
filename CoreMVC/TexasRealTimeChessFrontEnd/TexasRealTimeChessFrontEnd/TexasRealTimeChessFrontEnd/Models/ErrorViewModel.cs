ebff8aec7e7443254b8df8e20a741853d279ccef
blakepritchard <blakepritchard@yahoo.com>
2019-10-05 21:27:35 -0500

    New Project - Migrating Front End to .Net Core

---
+++ CoreMVC/TexasRealTimeChessFrontEnd/TexasRealTimeChessFrontEnd/TexasRealTimeChessFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TexasRealTimeChessFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- WebAPI/RealTimeChessAlphaSeven/RealTimeChessAlphaSeven/Data/Scripts/postPlayerTypes.bat
+++ WebAPI/RealTimeChessAlphaSeven/RealTimeChessAlphaSeven/Data/Scripts/postPlayerTypes.bat
@@ -2,3 +2,10 @@ curl -i -X POST -H “Content-Type: application/json” -d "{ 'PlayerTypeName': 
 curl -i -X POST -H “Content-Type: application/json” -d "{ 'PlayerTypeName': 'Black', 'TurnOrder': '2'}" https://texasrealtimechess.azurewebsites.net/api/playerTypes
 curl -i -X POST -H “Content-Type: application/json” -d "{ 'PlayerTypeName': 'Red', 'TurnOrder': '3'}" https://texasrealtimechess.azurewebsites.net/api/playerTypes
 curl -i -X POST -H “Content-Type: application/json” -d "{ 'PlayerTypeName': 'Blue', 'TurnOrder': '4'}" https://texasrealtimechess.azurewebsites.net/api/playerTypes
+
+
+curl -i -X POST -H “Content-Type: application/json” https://texasrealtimechess.azurewebsites.net/api/
+
+
+
+curl -i -X POST -H “Content-Type: application/json” -d "{}" https://texasrealtimechess.azurewebsites.net/api/ChessMatches/1/Setup
